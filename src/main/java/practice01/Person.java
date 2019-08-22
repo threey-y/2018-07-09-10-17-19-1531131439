407ff64d83c0117de58cdf9bb37ab9a218e15fb9
threey-y <threey.y@foxmail.com>
2019-08-23 00:00:33 +0800

    commit-01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,30 @@
 package practice01;
 
+import com.google.common.base.Strings;
+
 public class Person {
+//    写一个Person类，要有name，age属性，要有一个introduce方法，
+//    introduce方法返回一个字符串形如：
+//            >My name is Tom. I am 21 years old.
+
+    public String name;
+    public int age;
+
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String introduce(){
+        return ("My name is " + name + ". I am " + age + " years old.");
+    }
+
 }
