a4d9f124e3ac67562a0ce525bc8cf23ef8044037
threey-y <threey.y@foxmail.com>
2019-08-23 10:17:32 +0800

    commit-03-add worker-introduce

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,22 @@
 package practice04;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is "+this.name+". I am 21 years old";
+    }
 }
