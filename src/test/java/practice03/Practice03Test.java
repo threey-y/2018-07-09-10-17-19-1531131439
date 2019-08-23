e96d106399921ea28c1ca94005df1e6c859cce65
threey-y <threey.y@foxmail.com>
2019-08-23 09:43:29 +0800

    commit-03-add Person

--- src/test/java/practice03/Practice03Test.java
+++ src/test/java/practice03/Practice03Test.java
@@ -1,17 +1,17 @@
-//package practice03;
-//
-//import org.junit.Test;
-//
-//import static org.fest.assertions.api.Assertions.assertThat;
-//
-//public class Practice03Test {
-//    @Test
-//    public void should_person_have_name_and_age() throws Exception {
-//        Person person = new Person("Tom", 21);
-//        assertThat(person.getName()).isEqualTo("Tom");
-//        assertThat(person.getAge()).isEqualTo(21);
-//    }
-//
+package practice03;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class Practice03Test {
+    @Test
+    public void should_person_have_name_and_age() throws Exception {
+        Person person = new Person("Tom", 21);
+        assertThat(person.getName()).isEqualTo("Tom");
+        assertThat(person.getAge()).isEqualTo(21);
+    }
+
 //    @Test
 //    public void should_person_have_an_introduce_method_which_introduce_person_with_name_and_age() throws Exception {
 //        Person tom = new Person("Tom", 21);
@@ -45,4 +45,4 @@
 //        Worker tom = new Worker("Tom", 21);
 //        assertThat(tom.introduce()).isEqualTo("I am a Worker. I have a job.");
 //    }
-//}
+}
